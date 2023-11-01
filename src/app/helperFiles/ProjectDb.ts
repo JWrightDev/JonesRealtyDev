e339b0913b54099fa8d2580300349b70cfbdbab8
JWrightDev <jimmywright77@outlook.com>
2023-11-01 12:45:39 -0400

    Changed method of image serving
    Switched from local image storage to ImageKit. Improves loading times and file sizes.

--- src/app/helperFiles/ProjectDb.ts
+++ src/app/helperFiles/ProjectDb.ts
@@ -7,7 +7,7 @@ export const PROJECTLIST: Project[] = [
     projectName: '1191 Walker Road. McGregor, ON.',
     projectDescription: 'Coming Soon!',
     projectCategory: 'Office Space For Lease',
-    thumbnailLink: './assets/JonesRealtyLogo.png',
+    thumbnailLink: 'https://ik.imagekit.io/uqghswpnt/Jones%20Realty/JonesRealtyLogo.png',
     projectContactLink: ''
   },
   {
@@ -15,7 +15,7 @@ export const PROJECTLIST: Project[] = [
     projectName: 'Miss MJ Ship',
     projectDescription: 'Coming Soon!',
     projectCategory: 'Future Project',
-    thumbnailLink: './assets/JonesRealtyLogo.png',
+    thumbnailLink: 'https://ik.imagekit.io/uqghswpnt/Jones%20Realty/JonesRealtyLogo.png',
     projectContactLink: '',
     isNew: true,
   },
--- src/app/helperFiles/PropertyDB.ts
+++ src/app/helperFiles/PropertyDB.ts
@@ -6,7 +6,7 @@ export const PROPERTYLIST: Property[] = [
       address: 'St. Anthony\'s Lofts. Amherstburg, ON.',
       description: 'Lofts Available Soon!',
       contactLink: 'https://www.instagram.com/liveatthelofts/?hl=en',
-      thumbnailLink: './assets/Lofts/Loft-Front-View.JPG',
+      thumbnailLink: 'https://ik.imagekit.io/uqghswpnt/Jones%20Realty/Loft-Front-View.JPG?tr=w-600',
       altText: "a view of the front of a building.",
       isNew: true,
     },
@@ -15,7 +15,7 @@ export const PROPERTYLIST: Property[] = [
       address: 'Richmond Praedium, Amherstburg, ON.',
       description: 'Apartments Coming Soon!',
       contactLink: 'https://www.facebook.com/profile.php?id=61551698053689',
-      thumbnailLink: './assets/Richmond/Building-Render.jpg',
+      thumbnailLink: 'https://ik.imagekit.io/uqghswpnt/Jones%20Realty/Building-Render.jpg?tr=w-600',
       altText: "a render of the apartment building",
       isNew: true
     },
@@ -24,7 +24,7 @@ export const PROPERTYLIST: Property[] = [
         address: '365 Sandwich Street S. Amherstburg, ON.',
         description: 'Commercial Space available for lease.',
         contactLink: 'https://www.remax.ca/commercial/on/amherstburg-real-estate/365-sandwich-street-south-wp_idm73000004-25339051-lst',
-        thumbnailLink: './assets/365Sandwich.png',
+        thumbnailLink: 'https://ik.imagekit.io/uqghswpnt/Jones%20Realty/365Sandwich.png?tr=w-600',
         altText: "an empty parking lot in front of a building. "
     },
     {
@@ -32,7 +32,7 @@ export const PROPERTYLIST: Property[] = [
         address: 'Weston Apartments. Essex, ON.',
         description: '2 bedroom, 2 bathroom units available for rent.',
         contactLink: 'https://westonessex.com/',
-        thumbnailLink: './assets/weston.png',
+        thumbnailLink: 'https://ik.imagekit.io/uqghswpnt/Jones%20Realty/weston.png?tr=w-600',
         altText: "a parking lot with several cars parked in it. "
     },
     {
@@ -40,7 +40,7 @@ export const PROPERTYLIST: Property[] = [
         address: 'Amherstburg Yacht Club. Amherstburg, ON.',
         description: 'Boat wells available for rent',
         contactLink: 'https://www.facebook.com/amherstburgyachtclub',
-        thumbnailLink: './assets/ayc.jpeg',
+        thumbnailLink: 'https://ik.imagekit.io/uqghswpnt/Jones%20Realty/ayc.jpeg?tr=w-600',
         altText: "an aerial view of a marina with boats docked. "
     },
     {
@@ -48,7 +48,7 @@ export const PROPERTYLIST: Property[] = [
         address: 'River Canard Yacht Club. Amherstburg, ON.',
         description: 'Boat wells available for rent. Daily and seasonal launch passes available.',
         contactLink: 'https://www.facebook.com/amherstburgyachtclub',
-        thumbnailLink: './assets/rcyc.png',
+        thumbnailLink: 'https://ik.imagekit.io/uqghswpnt/Jones%20Realty/rcyc.jpg',
         altText: "a view of a boat dock from across the street. "
     },
     {
@@ -56,7 +56,7 @@ export const PROPERTYLIST: Property[] = [
       address: 'The Fort. Amherstburg, ON.',
       description: 'Multi-use complex center for sale',
       contactLink: 'http://www.johndalimonte.com/Agents/Listings-Detail.cfm?sBrokerCode=remaxpreferred&aid=87122&m=22022389&id=3353472',
-      thumbnailLink: './assets/fort.png',
+      thumbnailLink: 'https://ik.imagekit.io/uqghswpnt/Jones%20Realty/fort.png',
       altText: "an empty parking lot in front of a activity center."
     },
 ]
